9ffbeb1f2863f5a4909e03e2f77bf17fbd685de0
Noma <1181098979@qq.com>
2016-12-05 15:31:01 +0800

    sum=2+5+8+11+14+…，输入正整数n，求sum的前n项和。
    sum=2+5+8+11+14+…，输入正整数n，求sum的前n项和。

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication41
+{
+    class Program
+    {
+        static void Main ( string [ ] args )
+        {
+            int inputNum = Convert.ToInt32 ( Console.ReadLine ( ) );
+
+            int totalNum = 0;
+            for (int i = 1 ; i <= inputNum ; i++)
+            {
+                totalNum = totalNum + 3 * i -1;
+            }
+
+            Console.WriteLine ( totalNum );
+        }
+    }
+}
